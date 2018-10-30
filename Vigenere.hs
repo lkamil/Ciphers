e5f3bd5c797a9bdc6a6ae59eb27a5e8b81d23a44
lkamil <lkamil@protonmail.com>
2018-10-30 20:42:13 +0100

    add vigenere decryption

--- Vigenere.hs
+++ Vigenere.hs
@@ -1,15 +1,24 @@
 import Data.Char
 
+encryptText :: [Char] -> [Char] -> [Char]
+encryptText key text = vigenere encryptChar key text
+
+decryptText :: [Char] -> [Char] -> [Char]
+decryptText key text = vigenere decryptChar key text
+
+vigenere :: (Char -> Char -> Char) -> [Char] -> [Char] -> [Char] 
+vigenere direction key [] = []
+vigenere direction key text = direction (head $ cycle key) (head $ map toLower text) : (vigenere direction (drop 1 key') (drop 1 text))
+    where key' = cycle key
+
 encryptChar :: Char -> Char -> Char
 encryptChar key letter
     | result > 122 = chr $ result - 26
     | otherwise    = chr result
     where result   = (ord letter) - 97 + (ord key)
 
-encryptText key [] = []
-encryptText key text = encryptChar (head $ cycle key) (head $ map toLower text) : (encryptText (drop 1 key') (drop 1 text))
-    where key' = cycle key
-
-
-
-
+decryptChar :: Char -> Char -> Char
+decryptChar key letter
+    | result < 97 = chr $ result + 26
+    | otherwise = chr result
+    where result = (ord letter) + 97 - (ord key)
